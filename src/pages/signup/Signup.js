afca5bf04efc7110c25be1270bbceac6ebe20c14
xDamneDx <xdamnedx@gmail.com>
2022-05-10 16:13:33 +0300

    Added login and signup forms w/ styling

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,6 +1,48 @@
+import { useState } from "react";
+
 // Styles:
 import styles from "./Signup.module.css";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    setDisplayName("");
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <form className={styles["signup-form"]} onSubmit={submitHandler}>
+      <h2>Signup</h2>
+      <label>
+        <span>Display Name:</span>
+        <input
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>Email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <button className="btn">Signup</button>
+    </form>
+  );
 }
