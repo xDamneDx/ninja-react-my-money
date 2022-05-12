fed5c5b8978723b770e43b2a04143f5800528143
xDamneDx <xdamnedx@gmail.com>
2022-05-12 10:47:09 +0300

    Added login functionality

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
 
 // Styles:
 import styles from "./Login.module.css";
@@ -6,9 +7,11 @@ import styles from "./Login.module.css";
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isPending } = useLogin();
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    login(email, password);
     setEmail("");
     setPassword("");
   };
@@ -32,7 +35,13 @@ export default function Login() {
           value={password}
         />
       </label>
-      <button className="btn">Login</button>
+      {!isPending && <button className="btn">Login</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          Loading
+        </button>
+      )}
+      {error && <p>{error}</p>}
     </form>
   );
 }
