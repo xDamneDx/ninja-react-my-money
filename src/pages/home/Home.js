e5d85d207c63c687c9edf8b716a30c08084f62d9
xDamneDx <xdamnedx@gmail.com>
2022-05-12 17:12:26 +0300

    Added couple divs and TransactionForm component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,20 @@
+import { useAuthContext } from "../../hooks/useAuthContext";
+
 // Styles:
 import styles from "./Home.module.css";
 
+// Conponents:
+import TransactionForm from "./TransactionForm";
+
 export default function Home() {
-  return <div>Home</div>;
+  const { user } = useAuthContext();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>Transaction list</div>
+      <div className={styles.sidebar}>
+        <TransactionForm uid={user.uid} />
+      </div>
+    </div>
+  );
 }
